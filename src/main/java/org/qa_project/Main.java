6f507be6664c43cd2ad1491e4dfd79d2a76bbb82
OtherMoon / Yuriy Saneev <saneevyuriy@gmail.com>
2025-09-27 13:47:25 +0300

    init a project

---
+++ src/main/java/org/qa_project/Main.java
@@ -0,0 +1,7 @@
+package org.qa_project;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+}
